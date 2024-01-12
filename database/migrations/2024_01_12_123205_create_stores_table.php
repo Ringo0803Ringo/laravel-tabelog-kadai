85b26459e07fcefe6ce016777237f0d755867dbc
DokuDokuringo <153196181+Ringo0803Ringo@users.noreply.github.com>
2024-01-12 21:28:16 +0900

    migration

---
+++ database/migrations/2024_01_12_123205_create_stores_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image');
+            $table->text('description')->nullable();
+            $table->string('business_hour')->nullable();
+            $table->integer('price');
+            $table->integer('postal_code');
+            $table->string('address');
+            $table->integer('phone_number');
+            $table->string('holiday')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+};
