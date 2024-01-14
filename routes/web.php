423d7c2026aea4fffb5a27cb3750f7acf8f40395
DokuDokuringo <153196181+Ringo0803Ringo@users.noreply.github.com>
2024-01-14 16:02:21 +0900

    edit web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\UserController;
+use App\Http\Controllers\Auth\AdminController;
+use App\Http\Controllers\Auth\BookingController;
+use App\Http\Controllers\Auth\CategoryController;
+use App\Http\Controllers\Auth\FavoriteController;
+use App\Http\Controllers\Auth\ReviewController;
+use App\Http\Controllers\Auth\StoreController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,12 +26,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::resource('user', UserController::class);
+Route::resource('user', UserController::class)->only(['index','show','edit','update']);
 Route::resource('admin', AdminController::class);
-Route::resource('booking', BookingController::class);
-Route::resource('category', CategoryController::class);
-Route::resource('favorite', FavoriteController::class);
+Route::resource('booking', BookingController::class)->only(['index','show']);
+Route::resource('category', CategoryController::class)->only(['index','show']);
+Route::resource('favorite', FavoriteController::class)->only(['index','show']);
 Route::resource('review', ReviewController::class);
-Route::resource('store', StoreController::class);
+Route::resource('store', StoreController::class)->only(['index','show']);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
