c11ae596246abe39f8a04a7927630106e06311d4
DokuDokuringo <153196181+Ringo0803Ringo@users.noreply.github.com>
2024-03-09 12:32:04 +0900

    会社情報修正

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Company;
+use Illuminate\Support\Facades\DB;
 
 class CompanyController extends Controller
 {
     public function company() {
 
-        $companies = Company::all();
+        $companies = Company::find(1);
 
         return view('company', compact('companies'));
     }
--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -8,6 +8,9 @@
                 <div class="card-header">会社情報</div>
                 <div class="card-body">
                     <p class="mt-3">会社名：{{ $companies->name }}</p>
+                    <p class="mt-3">郵便番号：{{ $companies->postal_code }}</p>
+                    <p class="mt-3">所在地：{{ $companies->address }}</p>
+                    <p class="mt-3">電話番号：{{ $companies->phone_number }}</p>
                 </div>
             </div>
         </div>
